17afe5d860ae60d9e70d33a23cef00124c772713
Dustin Bozarth <dustin@airshp.com>
2011-09-01 16:30:50 -0500

    ..

--- jquery.feedToJSON.js
+++ jquery.feedToJSON.js
@@ -9,20 +9,19 @@
 		}
 		options = $.extend($.fn.feedToJson.defaults,options);
 		var url = options.yqlURL + options.yqlQS + "'" + encodeURIComponent(options.feed) + "'" + "&_nocache=" + options.cacheBuster;
-		var results = $.getJSON(url, function(data){  
+		return $.getJSON(url, function(data){  
 				//console.log(data.query.results);
 				data = data.query.results;
 				$.isFunction(callback) && callback(data);
 				$.isFunction(options.success) && options.success(data);
 			}); 
-		return results;
 	};
   
   //defaults
   $.fn.feedToJson.defaults = {
   	yqlURL : 'http://query.yahooapis.com/v1/public/yql',  
   	yqlQS : '?format=json&callback=?&q=select%20*%20from%20rss%20where%20url%3D',  
-  	feed:'http://instagr.am/tags/puppies/feed/recent.rss',
+  	feed:'http://instagr.am/tags/tacos/feed/recent.rss',
   	cachebuster: Math.floor((new Date().getTime()) / 1200 / 1000),
   	success:null
   };
